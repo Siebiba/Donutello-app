de836f039f695253a8291c0bebdbc060b05b2a59
Siebe <r0834360@student.thomasmore.be>
2023-06-07 20:49:29 +0200

    style: add styling to donut gallery

--- src/components/DonutOrders.vue
+++ src/components/DonutOrders.vue
@@ -125,7 +125,7 @@ const logout = () => {
 
 <template>
     <div class="btn-logout">
-        <a href="#" @click.prevent="logout">Log out</a>
+        <a href="#" @click.prevent="logout">Logout</a>
     </div>
 
     <div class="gallery">
@@ -136,38 +136,34 @@ const logout = () => {
 
                 <button class="btn-delete" @click="deleteDonut" v-bind:id="donutOrder._id">X</button>
 
-
-
-
-
-
-                <div class="donut-image" v-if="donutOrder.glaze === 'cherry'">
-                    <img src="../assets/Donut.png" alt="vanille" width="100" height="100">
+                <div class="donut-image" v-if="donutOrder.glaze === 'Cherry'">
+                    <img src="../assets/Donut-cherry.png" alt="cherry" width="100" height="100">
                 </div>
-                <div class="donut-image" v-else-if="color === 'pistache'">
-                    <img src="../assets/Donut.png" alt="vanille" width="100" height="100">
+                <div class="donut-image" v-else-if="donutOrder.glaze === 'Pistache'">
+                    <img src="../assets/Donut-pistache.png" alt="pistache" width="100" height="100">
                 </div>
-                <div class="donut-image" v-else-if="color === 'chocolate'">
-                    <img src="../assets/Donut.png" alt="vanille" width="100" height="100">
+                <div class="donut-image" v-else-if="donutOrder.glaze === 'Chocolate'">
+                    <img src="../assets/Donut-chocolate.png" alt="chocolate" width="100" height="100">
                 </div>
-                <div class="donut-image" v-else="color === 'none'">
-                    <img src="../assets/Donut.png" alt="vanille" width="100" height="100">
+                <div class="donut-image" v-else="donutOrder.glaze === 'none'">
+                    <img src="../assets/Donut.png" alt="test" width="100" height="100">
                 </div>
 
                 <div class="info">
 
                     <h2>{{ donutOrder.name }}</h2>
+                    <h3>{{ donutOrder.glaze }} Donut</h3>
 
 
-                    <button @click="showDetails">Meer info</button>
+                    <button class="btn-show" @click="showDetails">Meer info</button>
 
                     <div class="details" style="display: none;">
-                        <h3>Name of Designer</h3>
-                        <p>{{ donutOrder.username }}</p>
-                        <h3>Name of company</h3>
+                        <h4>Name of Designer</h4>
+                        <p>{{ donutOrder.user }}</p>
+                        <h4>Name of company</h4>
                         <p>{{ donutOrder.company }}</p>
-                        <h3>Choose of Glaze</h3>
-                        <p>{{ donutOrder.glaze }}</p>
+                       
+                      
                     </div>
                 </div>
             </li>
@@ -206,7 +202,7 @@ const logout = () => {
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
     margin: 1rem;
     width: 320px;
-    height: 400px;
+    height: 420px;
     border-radius: 20px;
 }
 
@@ -239,11 +235,40 @@ a {
     float: right;
 }
 
+h1 {
+    font-size: 2em;
+   font-weight: bold;
+
+}
+
 .btn-logout a:hover {
 
     background-color: #3ec7d1;
     color: #ffffff;
     cursor: pointer;
+}
+
+.btn-show {
+    font-size: 1em;
+    padding: 0.2em 1em;
+    font-weight: bold;
+    border-radius: 2em;
+    border: none;
+    color: white;
+    background-color: #e72870;
+    cursor: pointer;
+    margin-top: 10px;
+    
+}
+
+h3 {
+    font-size: 1em;
+    color:#e72870;
+}
+h2{
+    font-size: 1.4em;
+    color:black;
+    font-weight: bold;
 }
 
 .info {
@@ -254,35 +279,91 @@ a {
 
 .donut-image {
     margin: auto;
-    background-color: antiquewhite;
+    background-color: rgb(255, 235, 254);
     border-radius: 20px 20px 0 0;
+}
+
+.details {
+    
+    margin-top: 0.4rem;
+}
+
+.details h4 {
+    font-size: 1em;
+    color: #e72870;
+   
+}
+    
+.details p {
+    font-size: 1em;
+    color: black;
+    font-weight: bold;
 }
 
 .btn-delete {
     position: absolute;
     width: 30px;
     height: 30px;
-    background-color: red;
+    background-color: rgb(179, 32, 32);
     color: white;
     border-radius: 50%;
     border: none;
     padding: 0.5rem;
     cursor: pointer;
     margin: 0.5rem;
+    font-weight: bold;
+}
+
+.btn-delete:hover {
+    background-color: #cc13b3;
+    color: #ffffff;
+    cursor: pointer;
 }
 
 
 @media (max-width: 800px) {
-
+    h3 {
+    font-size: 0.8em;
+    color:#e72870;
+}
+h2{
+    font-size: 1.1em;
+    color:black;
+    font-weight: bold;
+}
     .donut-order {
         width: 200px;
-        height: 240px;
+        height: 300px;
     }
 
 
     .donut-order img {
         width: 120px;
     }
+
+    .details {
+    
+    margin-top: 0.2rem;
+}
+
+.details h4 {
+    font-size: 0.8em;
+    color: #e72870;
+   
+}
+    
+.details p {
+    font-size: 0.8em;
+    color: black;
+    font-weight: bold;
+}
+
+.info {
+    margin: 1rem;
+    position: absolute;
+    margin-top: 0.6rem;
+}
+
 }
 
 </style>
